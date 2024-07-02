8c7fe324b30766bd8ff3b7a46e1f2e7955c73b12
Nazareno <nazarenolrios@gmail.com>
2024-07-02 10:26:00 -0300

    mysql port

--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT || 54831,
   connectTimeout: 10000,
   ssl: {
     rejectUnauthorized: false,
